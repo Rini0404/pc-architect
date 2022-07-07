121947d5f848e372c128a3f92aa855f8596c14df
Rene Ortega <reneortega2468@gmail.com>
2022-07-07 18:55:50 -0400

    cards correspond to parts in favs.

--- frontend/src/pages/Favs.jsx
+++ frontend/src/pages/Favs.jsx
@@ -1,4 +1,12 @@
 import React from "react";
+import GpuPic from "../imgs/graphics-card.png";
+import CpuPic from "../imgs/cpu-tower.png";
+import RamPic from "../imgs/ram.png";
+import UsbPic from "../imgs/usb-drive.png";
+import HddPic from "../imgs/hard-disk.png";
+import SsdPic from "../imgs/ssd-drive.png";
+import { FaSearch } from "react-icons/fa";
+import { AiFillHeart } from "react-icons/ai";
 
 function Favorites() {
   const favs = JSON.parse(localStorage.getItem("fav"));
@@ -10,49 +18,247 @@ function Favorites() {
       <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3 md:-mx-8">
       {/* display favorites as cards */}
       {favs && favs.map((item) => {
-        return (
-          <div className="md:p-12 lg:p-8 xl:p-12 p-3">
-                <div
-                  class="relative block p-8 border border-gray-100 shadow-xl rounded-xl"
-                  href=""
-                >
-                  <span class="absolute right-4 top-4 rounded-full px-3 py-1.5 bg-green-100 text-green-600 font-medium text-xs">
-                  { item.Type}
-                  </span>
+        if (item.Type === "GPU") {
+          return (
+            <div className="md:p-12 lg:p-8 xl:p-12 p-3">
+              <div
+                className="relative block p-8 border border-gray-100 shadow-xl rounded-xl "
+                href=""
+              >
+                <span className="absolute right-4 top-4 rounded-full px-3 py-1.5 bg-green-100 text-green-600 font-medium text-xs">
+                  {item.Type}
+                </span>
 
-                  <div class="mt-4 text-gray-500 sm:pr-8">
-                    <svg
-                      
-                      className="w-8 h-8 sm:w-10 sm:h-10"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      strokeWidth={2}
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z"
-                      />
-                    </svg>
+                <div className="mt-4 text-gray-500 sm:pr-8">
+                  <img className="w-8 h-8 sm:w-10 sm:h-10" src={GpuPic} />
+                  <h5 className="mt-4 text-lg	 font-bold text-gray-900">
+                    {item.Model}
+                  </h5>
 
-                    <h5 class="mt-4 text-lg	 font-bold text-gray-900">
-                      {item.Model}
-                    </h5>
-
-                    <p class=" mt-2 text-sm sm:block text-yellow-700">
-                      {item.Brand}
-                    </p>
-                    <p class=" mt-2 text-sm sm:block">
-                    Gpu Rank: {item.Rank}<br></br>
-                    Benchmark: {item.Benchmark}<br></br>
+                  <p className=" mt-2 text-sm sm:block text-yellow-700">
+                    {item.Brand}
+                  </p>
+                  <p className=" mt-2 text-sm sm:block">
+                    Gpu Rank: {item.Rank}
+                    <br></br>
+                    Benchmark: {item.Benchmark}
+                    <br></br>
                     Samples: {item.Samples}
-                    </p>
+                  </p>
+
+                  {/* add button to the bottom right of card  */}
+                  <div className="flex justify-end">
+                    <button
+                      className="bg-indigo-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+                    >
+                      <AiFillHeart />
+                    </button>
                   </div>
                 </div>
-                
               </div>
-        )
+            </div>
+          );
+        } else if (item.Type === "CPU") {
+          return (
+            <div className="md:p-12 lg:p-8 xl:p-12 p-3">
+              <div
+                className="relative block p-8 border border-gray-100 shadow-xl rounded-xl"
+                href=""
+              >
+                <span className="absolute right-4 top-4 rounded-full px-3 py-1.5 bg-green-100 text-green-600 font-medium text-xs">
+                  {item.Type}
+                </span>
+
+                <div className="mt-4 text-gray-500 sm:pr-8">
+                  <img className="w-8 h-8 sm:w-10 sm:h-10" src={CpuPic} />
+                  <h5 className="mt-4 text-lg	 font-bold text-gray-900">
+                    {item.Model}
+                  </h5>
+
+                  <p className=" mt-2 text-sm sm:block text-yellow-700">
+                    {item.Brand}
+                  </p>
+                  <p className=" mt-2 text-sm sm:block">
+                    Cpu Rank: {item.Rank}
+                    <br></br>
+                    Benchmark: {item.Benchmark}
+                    <br></br>
+                    Samples: {item.Samples}
+                  </p>
+
+                  {/* add button to the bottom right of card  */}
+                  <div className="flex justify-end">
+                    <button
+                      className="bg-indigo-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+                    >
+                      <AiFillHeart />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        } else if (item.Type === "RAM") {
+          return (
+            <div className="md:p-12 lg:p-8 xl:p-12 p-3">
+              <div
+                className="relative block p-8 border border-gray-100 shadow-xl rounded-xl"
+                href=""
+              >
+                <span className="absolute right-4 top-4 rounded-full px-3 py-1.5 bg-green-100 text-green-600 font-medium text-xs">
+                  {item.Type}
+                </span>
+
+                <div className="mt-4 text-gray-500 sm:pr-8">
+                  <img className="w-8 h-8 sm:w-10 sm:h-10" src={RamPic} />
+                  <h5 className="mt-4 text-lg	 font-bold text-gray-900">
+                    {item.Model}
+                  </h5>
+
+                  <p className=" mt-2 text-sm sm:block text-yellow-700">
+                    {item.Brand}
+                  </p>
+                  <p className=" mt-2 text-sm sm:block">
+                    Ram Rank: {item.Rank}
+                    <br></br>
+                    Benchmark: {item.Benchmark}
+                    <br></br>
+                    Samples: {item.Samples}
+                  </p>
+
+                  {/* add button to the bottom right of card  */}
+                  <div className="flex justify-end">
+                    <button
+                      className="bg-indigo-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+                    >
+                      <AiFillHeart />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        } else if (item.Type === "SSD") {
+          return (
+            <div className="md:p-12 lg:p-8 xl:p-12 p-3">
+              <div
+                className="relative block p-8 border border-gray-100 shadow-xl rounded-xl"
+                href=""
+              >
+                <span className="absolute right-4 top-4 rounded-full px-3 py-1.5 bg-green-100 text-green-600 font-medium text-xs">
+                  {item.Type}
+                </span>
+
+                <div className="mt-4 text-gray-500 sm:pr-8">
+                  <img className="w-8 h-8 sm:w-10 sm:h-10" src={SsdPic} />
+                  <h5 className="mt-4 text-lg	 font-bold text-gray-900">
+                    {item.Model}
+                  </h5>
+
+                  <p className=" mt-2 text-sm sm:block text-yellow-700">
+                    {item.Brand}
+                  </p>
+                  <p className=" mt-2 text-sm sm:block">
+                    Ssd Rank: {item.Rank}
+                    <br></br>
+                    Benchmark: {item.Benchmark}
+                    <br></br>
+                    Samples: {item.Samples}
+                  </p>
+
+                  {/* add button to the bottom right of card  */}
+                  <div className="flex justify-end">
+                    <button
+                      className="bg-indigo-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+                    >
+                      <AiFillHeart />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        } else if (item.Type === "USB") {
+          return (
+            <div className="md:p-12 lg:p-8 xl:p-12 p-3">
+              <div
+                className="relative block p-8 border border-gray-100 shadow-xl rounded-xl"
+                href=""
+              >
+                <span className="absolute right-4 top-4 rounded-full px-3 py-1.5 bg-green-100 text-green-600 font-medium text-xs">
+                  {item.Type}
+                </span>
+
+                <div className="mt-4 text-gray-500 sm:pr-8">
+                  <img className="w-8 h-8 sm:w-10 sm:h-10" src={UsbPic} />
+                  <h5 className="mt-4 text-lg	 font-bold text-gray-900">
+                    {item.Model}
+                  </h5>
+
+                  <p className=" mt-2 text-sm sm:block text-yellow-700">
+                    {item.Brand}
+                  </p>
+                  <p className=" mt-2 text-sm sm:block">
+                    Usb Rank: {item.Rank}
+                    <br></br>
+                    Benchmark: {item.Benchmark}
+                    <br></br>
+                    Samples: {item.Samples}
+                  </p>
+
+                  {/* add button to the bottom right of card  */}
+                  <div className="flex justify-end">
+                    <button
+                      className="bg-indigo-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+                    >
+                      <AiFillHeart />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        } else if (item.Type === "HDD") {
+          return (
+            <div className="md:p-12 lg:p-8 xl:p-12 p-3">
+              <div
+                className="relative block p-8 border border-gray-100 shadow-xl rounded-xl"
+                href=""
+              >
+                <span className="absolute right-4 top-4 rounded-full px-3 py-1.5 bg-green-100 text-green-600 font-medium text-xs">
+                  {item.Type}
+                </span>
+
+                <div className="mt-4 text-gray-500 sm:pr-8">
+                  <img className="w-8 h-8 sm:w-10 sm:h-10" src={HddPic} />
+                  <h5 className="mt-4 text-lg	 font-bold text-gray-900">
+                    {item.Model}
+                  </h5>
+
+                  <p className=" mt-2 text-sm sm:block text-yellow-700">
+                    {item.Brand}
+                  </p>
+                  <p className=" mt-2 text-sm sm:block">
+                    Hdd Rank: {item.Rank}
+                    <br></br>
+                    Benchmark: {item.Benchmark}
+                    <br></br>
+                    Samples: {item.Samples}
+                  </p>
+
+                  {/* add button to the bottom right of card  */}
+                  <div className="flex justify-end">
+                    <button
+                      className="bg-indigo-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+                    >
+                      <AiFillHeart />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        }
       }
       )}
 
