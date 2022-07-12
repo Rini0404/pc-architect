e43cfb735c90167d98ae1347a252d85bdf29335b
Rene Ortega <reneortega2468@gmail.com>
2022-07-11 20:00:39 -0400

    Heroku Deploy

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require("express");
 const dotenv = require("dotenv").config();
 const colors = require("colors");
@@ -15,7 +16,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/api/", require("./routes/gpuRoutes"));
 app.use('/api/users', require('./routes/userRoutes'));
 
-
+// serve frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html'));
+  }
+  );
+} else {
+  app.get('/', (req, res) => res.send('Please set to production'));
+    }
 
 app.use(errorHandler);
 
