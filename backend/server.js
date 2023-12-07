8015ab1a13eac93cfd61ff5ea8b1bb6384280eb7
Rini <Reneortega2468@gmail.com>
2023-12-07 10:12:32 -0500

    Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -23,9 +23,7 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html'));
   }
   );
-} else {
-  app.get('/', (req, res) => res.send('Please set to production'));
-}
+} 
 
 app.use(errorHandler);
 
