2d05e672ce2c972b4e7c986a94ae12b130d426a0
Rini <Reneortega2468@gmail.com>
2023-12-07 10:35:36 -0500

    Prep heroku push

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,9 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html'));
   }
   );
-} 
+} else {
+  app.get('/', (req, res) => res.send('Please set to production'));
+}
 
 app.use(errorHandler);
 
